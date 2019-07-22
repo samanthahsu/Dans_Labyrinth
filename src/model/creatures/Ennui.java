bbd9d947446433fe77948dafa4bee486afd6683d
sam7891 <shsu877@gmail.com>
2019-07-22 08:43:34 -0700

    pizza tests pazz

--- src/model/creatures/Ennui.java
+++ src/model/creatures/Ennui.java
@@ -1,5 +1,43 @@
 package model.creatures;
 
+import model.Map;
+
 /*the one that tries to run away*/
-public class Ennui {
+/*if within 2 tiles of dan give indication of faint sound
+* if within 1 tile, give loud sound indication*/
+public class Ennui extends Creature {
+
+    boolean hasKey;
+    /*effects: set coordinates, name, and description*/
+    public Ennui(int y, int x) {
+        super(y, x);
+        name = "ennui";
+        description = "a flash of turquoise fuzz in the dark";
+        hasKey = true;
+    }
+
+    @Override
+    void move(Map map) {
+
+    }
+
+    @Override
+    void attack(Map map) {
+
+    }
+
+    @Override
+    void speak() {
+
+    }
+
+    @Override
+    public void doPassiveActions() {
+
+    }
+
+    @Override
+    public boolean interact(Map map) {
+        return false;
+    }
 }
--- src/model/items/Note.java
+++ src/model/items/Note.java
@@ -5,7 +5,11 @@ import model.Map;
 /*note that is at the beginning in the same tile as ava*/
 public class Note extends Item {
 
-
+    public Note(int y, int x) {
+        super(y, x);
+        name = "note";
+        description = "An address is scrawled on with neat handwriting. Note to self:";
+    }
 
     @Override
     public boolean interact(Map map) {
